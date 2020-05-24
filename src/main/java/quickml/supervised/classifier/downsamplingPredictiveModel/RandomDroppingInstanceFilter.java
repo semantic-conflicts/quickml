bbfa75ab84d814aae2cea63c9cec6241229362f2
semantic conflicts <semantic.conflicts@gmail.com>
2020-05-24 19:58:16 -0300

    'Trigger build #360f6c35'

--- src/main/java/quickml/supervised/classifier/downsamplingPredictiveModel/RandomDroppingInstanceFilter.java
+++ src/main/java/quickml/supervised/classifier/downsamplingPredictiveModel/RandomDroppingInstanceFilter.java
@@ -1,31 +1,28 @@
 package quickml.supervised.classifier.downsamplingPredictiveModel;
-
 import com.google.common.base.Predicate;
 import quickml.supervised.Misc;
 import quickml.data.Instance;
-
 import java.io.Serializable;
 import java.util.Map;
-
-/**
+/** 
  * Created by ian on 4/23/14.
  */
-class RandomDroppingInstanceFilter implements Predicate<Instance<Map<String, Serializable>>> {
-    private final Serializable classificationToDrop;
-    private final double dropProbability;
-
-    public RandomDroppingInstanceFilter(Serializable classificationToDrop, double dropProbability) {
-        this.classificationToDrop = classificationToDrop;
-        this.dropProbability = dropProbability;
+class RandomDroppingInstanceFilter implements Predicate<Instance<Map<String,Serializable>>> {
+  public Serializable classificationToDrop;
+  public double dropProbability;
+  public RandomDroppingInstanceFilter(  Serializable classificationToDrop,  double dropProbability){
+    this.classificationToDrop=classificationToDrop;
+    this.dropProbability=dropProbability;
+  }
+  @Override public boolean apply(  final Instance instance){
+    if (instance.getLabel().equals(classificationToDrop)) {
+      final double rand=Misc.random.nextDouble();
+      return rand > dropProbability;
     }
-
-    @Override
-    public boolean apply(final Instance instance) {
-        if (instance.getLabel().equals(classificationToDrop)) {
-            final double rand = Misc.random.nextDouble();
-            return rand > dropProbability;
-        } else {
-            return true;
-        }
+ else {
+      return true;
     }
+  }
+  public RandomDroppingInstanceFilter(){
+  }
 }
