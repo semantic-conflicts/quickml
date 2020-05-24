94c73018e0bf8c1a795b79feca5285164fab3064
semantic conflicts <semantic.conflicts@gmail.com>
2020-05-24 19:58:10 -0300

    'Trigger build #49db26bebcff86c52de7d6206ad0e2eca7ac264f'

--- src/main/java/quickml/supervised/classifier/downsamplingPredictiveModel/RandomDroppingInstanceFilter.java
+++ src/main/java/quickml/supervised/classifier/downsamplingPredictiveModel/RandomDroppingInstanceFilter.java
@@ -1,30 +1,27 @@
 package quickml.supervised.classifier.downsamplingPredictiveModel;
-
 import com.google.common.base.Predicate;
 import quickml.supervised.Misc;
 import quickml.data.Instance;
-
 import java.io.Serializable;
-
-/**
+/** 
  * Created by ian on 4/23/14.
  */
 class RandomDroppingInstanceFilter implements Predicate<Instance> {
-    private final Serializable classificationToDrop;
-    private final double dropProbability;
-
-    public RandomDroppingInstanceFilter(Serializable classificationToDrop, double dropProbability) {
-        this.classificationToDrop = classificationToDrop;
-        this.dropProbability = dropProbability;
+  public Serializable classificationToDrop;
+  public double dropProbability;
+  public RandomDroppingInstanceFilter(  Serializable classificationToDrop,  double dropProbability){
+    this.classificationToDrop=classificationToDrop;
+    this.dropProbability=dropProbability;
+  }
+  @Override public boolean apply(  final Instance Instance){
+    if (Instance.getLabel().equals(classificationToDrop)) {
+      final double rand=Misc.random.nextDouble();
+      return rand > dropProbability;
     }
-
-    @Override
-    public boolean apply(final Instance Instance) {
-        if (Instance.getLabel().equals(classificationToDrop)) {
-            final double rand = Misc.random.nextDouble();
-            return rand > dropProbability;
-        } else {
-            return true;
-        }
+ else {
+      return true;
     }
+  }
+  public RandomDroppingInstanceFilter(){
+  }
 }
