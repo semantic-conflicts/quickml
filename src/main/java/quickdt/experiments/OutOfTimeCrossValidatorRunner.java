442db153b7c469b98f0c6b101e7f362ed8602ea9
semantic conflicts <semantic.conflicts@gmail.com>
2020-05-24 23:34:50 -0300

    'Trigger build #bae968d9a85c0501ba8842a6f88e7fc2c6b78693'

--- src/main/java/quickdt/experiments/OutOfTimeCrossValidatorRunner.java
+++ src/main/java/quickdt/experiments/OutOfTimeCrossValidatorRunner.java
@@ -1,5 +1,4 @@
 package quickdt.experiments;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import quickdt.crossValidation.*;
@@ -7,41 +6,33 @@ import quickdt.data.AbstractInstance;
 import quickdt.predictiveModels.PredictiveModelBuilder;
 import quickdt.predictiveModels.decisionTree.TreeBuilder;
 import quickdt.predictiveModels.randomForest.RandomForestBuilder;
-
 import java.util.List;
-
-/**
+/** 
  * Created by alexanderhawk on 1/16/14.
  */
 public class OutOfTimeCrossValidatorRunner {
-    private static final Logger logger =  LoggerFactory.getLogger(StationaryCrossValidator.class);
-
-
-    public static void main(String[] args) {
-        int numTraniningExamples = 40100;
-        String bidRequestAttributes[] = {"seller_id", "user_id", "users_favorite_beer_id", "favorite_soccer_team_id", "user_iq"};
-        TrainingDataGenerator2 trainingDataGenerator = new TrainingDataGenerator2(numTraniningExamples, .005, bidRequestAttributes);
-        List<AbstractInstance> trainingData = trainingDataGenerator.createTrainingData();
-        int millisInMinute = 60000;
-        int instanceNumber = 0;
-        for (AbstractInstance instance : trainingData) {
-            instance.getAttributes().put("currentTimeMillis", millisInMinute * instanceNumber);
-            instanceNumber++;
-        }
-        logger.info("trainingDataSize " + trainingData.size());
-        PredictiveModelBuilder predictiveModelBuilder = getRandomForestBuilder(5, 5);
-        CrossValidator crossValidator = new OutOfTimeCrossValidator(new NonWeightedAUCCrossValLoss(), 0.25, 30, new TestDateTimeExtractor()); //number of validation time slices
-
-        double totalLoss = crossValidator.getCrossValidatedLoss(predictiveModelBuilder, trainingData);
-
+  public static Logger logger=LoggerFactory.getLogger(StationaryCrossValidator.class);
+  public static void main(  String[] args){
+    int numTraniningExamples=40100;
+    String bidRequestAttributes[]={"seller_id","user_id","users_favorite_beer_id","favorite_soccer_team_id","user_iq"};
+    TrainingDataGenerator2 trainingDataGenerator=new TrainingDataGenerator2(numTraniningExamples,.005,bidRequestAttributes);
+    List<AbstractInstance> trainingData=trainingDataGenerator.createTrainingData();
+    int millisInMinute=60000;
+    int instanceNumber=0;
+    for (    AbstractInstance instance : trainingData) {
+      instance.getAttributes().put("currentTimeMillis",millisInMinute * instanceNumber);
+      instanceNumber++;
     }
-
-    private static PredictiveModelBuilder getRandomForestBuilder(int maxDepth, int numTrees) {
-
-        TreeBuilder treeBuilder = new TreeBuilder().maxDepth(maxDepth).ignoreAttributeAtNodeProbability(.7).minLeafInstances(20);
-        RandomForestBuilder RandomForestBuilder = new RandomForestBuilder(treeBuilder).numTrees(numTrees);
-        return RandomForestBuilder;
-
-    }
-
+    logger.info("trainingDataSize " + trainingData.size());
+    PredictiveModelBuilder predictiveModelBuilder=getRandomForestBuilder(5,5);
+    CrossValidator crossValidator=new OutOfTimeCrossValidator(new NonWeightedAUCCrossValLoss(),0.25,30,new TestDateTimeExtractor());
+    double totalLoss=crossValidator.getCrossValidatedLoss(predictiveModelBuilder,trainingData);
+  }
+  public static PredictiveModelBuilder getRandomForestBuilder(  int maxDepth,  int numTrees){
+    TreeBuilder treeBuilder=new TreeBuilder().maxDepth(maxDepth).ignoreAttributeAtNodeProbability(.7).minLeafInstances(20);
+    RandomForestBuilder RandomForestBuilder=new RandomForestBuilder(treeBuilder).numTrees(numTrees);
+    return RandomForestBuilder;
+  }
+  public OutOfTimeCrossValidatorRunner(){
+  }
 }
